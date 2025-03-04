6fe9fd60621fa3bff8317ba9e55cedeb83321c15
HxrxxH <113428265+HxrxxH@users.noreply.github.com>
2025-03-04 15:55:17 +0530

    Add files via upload

---
+++ RSA/RSA.py
@@ -0,0 +1,46 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+def generate_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_data(public_key, message):
+    encrypted = public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+def decrypt_data(private_key, encrypted_message):
+    decrypted = private_key.decrypt(
+        encrypted_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted.decode()
+
+# Generate RSA keys
+private_key, public_key = generate_keys()
+
+# Message to encrypt
+message = "Hello, this is a secret message!"
+
+# Encrypt the message
+encrypted_message = encrypt_data(public_key, message)
+print("Encrypted message:", encrypted_message)
+
+# Decrypt the message
+decrypted_message = decrypt_data(private_key, encrypted_message)
+print("Decrypted message:", decrypted_message)
